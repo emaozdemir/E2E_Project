3dc7992ce9d92e75b221881b343395fdf81fde88
emaozdemir <semaozdemiraslan@gmail.com>
2024-08-14 20:17:47 +0200

    first commit

---
+++ src/test/java/stepdefinitions/ContactStepDefs.java
@@ -0,0 +1,24 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ContactStepDefs {
+    @Given("set the url for adding contact")
+    public void setTheUrlForAddingContact() {
+    }
+
+    @And("set the expected data for adding contact")
+    public void setTheExpectedDataForAddingContact() {
+    }
+
+    @When("send the post request for adding contact")
+    public void sendThePostRequestForAddingContact() {
+    }
+
+    @Then("do assertion for adding contact")
+    public void doAssertionForAddingContact() {
+    }
+}
